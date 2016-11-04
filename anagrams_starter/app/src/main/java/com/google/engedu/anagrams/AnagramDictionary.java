d162afa5843022474f88e5dcfd45f5599495f1d4
darshan <darshan.gowda008@gmail.com>
2016-11-05 00:59:49 +0530

    unit 1 - starter code added

---
+++ anagrams_starter/app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,42 @@
+package com.google.engedu.anagrams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private Random random = new Random();
+
+    public AnagramDictionary(InputStream wordListStream) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        String line;
+        while((line = in.readLine()) != null) {
+            String word = line.trim();
+        }
+    }
+
+    public boolean isGoodWord(String word, String base) {
+        return true;
+    }
+
+    public ArrayList<String> getAnagrams(String targetWord) {
+        ArrayList<String> result = new ArrayList<String>();
+        return result;
+    }
+
+    public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+        return result;
+    }
+
+    public String pickGoodStarterWord() {
+        return "stop";
+    }
+}
