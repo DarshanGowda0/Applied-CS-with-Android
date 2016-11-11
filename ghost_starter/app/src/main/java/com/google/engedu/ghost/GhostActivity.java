cac3a1e6ebb273074f9736edf5acc664c3db9bd1
darshan <darshan.gowda008@gmail.com>
2016-11-11 21:01:21 +0530

    Ui implementation

--- ghost_starter/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
+++ ghost_starter/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -45,10 +45,9 @@ public class GhostActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        
         int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
